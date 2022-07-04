d6f8116eb36dea1884715303169483b35fef594d
Néliton Vanderley <neliton7+dev@gmail.com>
2022-07-03 23:00:52 -0300

    Update bomba-contagem-regressiva.py

--- bomba-contagem-regressiva.py
+++ bomba-contagem-regressiva.py
@@ -1,14 +1,18 @@
 import time
 tempo = int(input('Informe o tempo para a contagem regressiva: '))
+print('\n\nAtenção! \nIniciando contagem regressiva... ')
+time.sleep(1)
 def contagem_regressiva(tempo):
-  print('\n\nAtenção! \nIniciando contagem regressiva... ')
-  time.sleep(1)
-  for i in range(tempo, -1, -1):
-    if i == 0:
-      print("Buuum!!!!  \nA bomba explodiu!!!!") 
-      break
-    print(i)
-    time.sleep(1)
-contagem_regressiva(tempo)
+	for i in range(tempo, -1, -1):
+		if i == 0:
+			explosao = True
+			break
+		print(i)
+		time.sleep(1)
+	return explosao
+
+if contagem_regressiva(tempo) == True:
+	print("Buuum!!!!  \nA bomba explodiu!!!!") 
+
 
 #Realiza a contagem regressiva para a explosão de uma bomba!
