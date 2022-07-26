abadab9de4e791d5e89c56c6b08864df755a679d
Néliton Vanderley <neliton7+dev@gmail.com>
2022-07-25 23:25:12 -0300

    Create At02-Configurando_o_ambiente_JS.js

---
+++ Javascript/At02-Configurando_o_ambiente_JS.js
@@ -0,0 +1,12 @@
+//Utilizei o VS Code devido a sua facilidade de utilização e a grande quantidade de usuários.
+//Adicionei uma extensão que deixa as cores de variáveis, funções, classes entre outros elementos com cores diferentes facilitando a identificação.
+var teste = 01;
+
+class testando (a, b){
+	function nome (teste) {
+		if (1 == 3){
+			var teste2 = false
+		}
+		else { var teste2 = true}
+		}
+	}
