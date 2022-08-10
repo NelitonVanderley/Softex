04e36e18f917b793bc7fb9886ab00452ede73cf5
Néliton Vanderley <neliton7+dev@gmail.com>
2022-08-10 11:48:42 -0300

    Update At11-Estruturas_de_repeticao.js
    Feito na Softex.

--- Javascript/At11-Estruturas_de_repeticao.js
+++ Javascript/At11-Estruturas_de_repeticao.js
@@ -4,16 +4,22 @@ function Carro (marca, ano, valor){
 	this.valor = valor
 }
 
+var palio = new Carro('Fiat', 2006, 15000)
 var ferrari = new Carro('Ferrari', 2020, 1000000)
-var gol = new Carro('Gol', 2012, 10000)
-var fusca = new Carro('Fusca', 1989,1000)
+var gol = new Carro('Volkswagen', 2012, 10000)
+var fusca = new Carro('Volkswagen', 1989,1000)
 
-var marcas = ['ferrari', 'gol', 'fusca']
+var marcas = [ferrari.marca, gol.marca, fusca.marca, palio.marca]
 
-var exibirObjetos = (Carro) => {
+var exibirParametros = () => {
 	for (var c in ferrari) {
-		console.log(c)
-	}  
-
-
+		console.log('Propriedade do objeto: ' + c)
+	} 
 }
+var exibiArray = () => {
+    for ( var array of marcas){
+        console.log('Marca: ' + array)
+    }
+}
+exibirParametros()
+exibiArray()
